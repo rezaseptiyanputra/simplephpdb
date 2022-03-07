192c528e72a106c260dba4e83809acb2702a749e
rezaseptiyanputra <63850950+rezaseptiyanputra@users.noreply.github.com>
2022-03-07 15:39:18 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "mariadb";
+$username = "root";
+$password = "P@ssw0rd";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+?>
